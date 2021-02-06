9ab1e214c081358e7fd7f51bdd3fcb5a509476a6
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-02-07 00:07:14 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -12,30 +12,24 @@ printf("Enter numerator followed by the denominator: ");
 scanf("%d%d", &a.n, &a.d);
 return a;
 }
-frac add(frac a[], int n)
+void add(frac a[], int n)
 {
 frac sum; int x;
-sum.n = sum.d = 0;
-for(int i=0;i<n-1;i++)
+sum.n = a[0].n;
+sum.d = a[0].d;
+for(int i=1;i<n;i++)
 {
-sum.n += (a[i].n * a[i+1].d) + (a[i+1].n * a[i].d);
-sum.d += a[i].d * a[i+1].d;
+sum.n = (sum.n * a[i].d) + (a[i].n * sum.d);
+sum.d *= a[i].d;
 }
-if(sum.n > sum.d)
-{x = sum.d;}
-else
-{x = sum.n;}
-while(x > 1)
+for(int i=1;i <= sum.n && i <= sum.d;i++)
 {
-if(sum.n > x && sum.n % x == 0 && sum.d > x && sum.d % x == 0)
-{
+if(sum.n % i == 0 && sum.d % i == 0)
+x = i;
+}
 sum.n /= x;
 sum.d /= x;
-}
-else
-x--;
-}
-return sum;
+printf("The sum of the fractions is %d/%d\n", sum.n, sum.d);
 }
 int main()
 {
@@ -47,7 +41,6 @@ for(int i=0;i<n;i++)
 {
 a[i] = getfrac();
 }
-sum = add(a, n);
-printf("The sum of the fractions is %d/%d\n", sum.n, sum.d);
+add(a, n);
 return 0;
 }
