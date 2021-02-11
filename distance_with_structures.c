af4cf0a48921f74536dfdc6ff864190d9f04ba19
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-02-11 16:18:14 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -3,31 +3,31 @@
 #include<math.h>
 typedef struct pair
 {
-int x;
-int y;
+    int x;
+    int y;
 }pair;
 float dist(pair a, pair b)
 {
-return sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2));
+    return sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2));
 }
 pair getpair()
 {
-pair a;
-printf("Enter a pair of coordinates(x and y): ");
-scanf("%d%d",&a.x, &a.y);
-return a;
+    pair a;
+    printf("Enter a pair of coordinates(x and y): ");
+    scanf("%d%d",&a.x, &a.y);
+    return a;
 }
 void disp(float d)
 {
-printf("The distance between the two points is %f\n", d);
+    printf("The distance between the two points is %f\n", d);
 }
 int main()
 {
-float distance;
-pair a, b;
-a = getpair();
-b = getpair();
-distance = dist(a, b);
-disp(distance);
-return 0;
+    float distance;
+    pair a, b;
+    a = getpair();
+    b = getpair();
+    distance = dist(a, b);
+    disp(distance);
+    return 0;
 }
