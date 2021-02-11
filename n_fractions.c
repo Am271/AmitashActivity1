18f57d80178b4dead4d0b098fa1140d156979dcd
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-02-11 16:13:53 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -2,45 +2,61 @@
 #include<stdio.h>
 typedef struct fraction
 {
-int n;
-int d;
+    int n;
+    int d;
 }frac;
 frac getfrac()
 {
-frac a;
-printf("Enter numerator followed by the denominator: ");
-scanf("%d%d", &a.n, &a.d);
-return a;
+    frac a;
+    printf("Enter numerator followed by the denominator: ");
+    scanf("%d%d", &a.n, &a.d);
+    return a;
 }
-void add(frac a[], int n)
+int inputfrac(frac a[])
 {
-frac sum; int x;
-sum.n = a[0].n;
-sum.d = a[0].d;
-for(int i=1;i<n;i++)
-{
-sum.n = (sum.n * a[i].d) + (a[i].n * sum.d);
-sum.d *= a[i].d;
+    int n;
+    printf("Enter number of fractions: ");
+    scanf("%d", &n);
+    for(int i=0;i<n;i++)
+    {
+        a[i] = getfrac();
+    }
+    return n;
 }
-for(int i=1;i <= sum.n && i <= sum.d;i++)
+frac add(frac a[], int n)
 {
-if(sum.n % i == 0 && sum.d % i == 0)
-x = i;
+    frac sum; int x;
+    sum.n = a[0].n;
+    sum.d = a[0].d;
+    for(int i=1;i<n;i++)
+    {
+        sum.n = (sum.n * a[i].d) + (a[i].n * sum.d);
+        sum.d *= a[i].d;
+    }
+    for(int i=1;i <= sum.n && i <= sum.d;i++)
+    {
+        if(sum.n % i == 0 && sum.d % i == 0)
+            x = i;
+    }
+    sum.n /= x;
+    sum.d /= x;
+    return sum;
 }
-sum.n /= x;
-sum.d /= x;
-printf("The sum of the fractions is %d/%d\n", sum.n, sum.d);
+void disp(frac a[], frac b, int n)
+{
+    int i = 0;
+    while(i<n-1)
+    {
+        printf("%d/%d + ", a[i].n, a[i].d);
+        i++;
+    }
+    printf("%d/%d = %d/%d", a[n-1].n, a[n-1].d, b.n, b.d);
 }
 int main()
 {
-int n; frac sum;
-printf("Enter number of fractions: ");
-scanf("%d", &n);
-frac a[n];
-for(int i=0;i<n;i++)
-{
-a[i] = getfrac();
-}
-add(a, n);
-return 0;
+    frac a[100], sum;
+    int n = inputfrac(a);
+    sum = add(a, n);
+    disp(a, sum, n);
+    return 0;
 }
