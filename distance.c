42960d05fd0d6b327adac6e03c2862ee1d37d932
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-02-12 11:59:37 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -3,28 +3,24 @@
 #include<math.h>
 float dist(float x1, float x2, float y1, float y2)
 {
-return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-}
-float getval()
-{
-float val;
-printf("Enter coordinate: ");
-scanf("%f",&val);
-return val;
+    return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
 void disp(float d)
 {
-printf("The distance between the two points is %f\n", d);
+    printf("The distance between the two points is %f\n", d);
+}
+void getval(float *x1, float *x2, float *y1, float *y2)
+{
+    printf("Enter the first pair of coordinates(x and y): ");
+    scanf("%f%f", x1, y1);
+    printf("Enter the second pair of coordinates(x and y): ");
+    scanf("%f%f", x2, y2);
 }
 int main()
 {
-float x1, x2, y1, y2, distance;
-printf("Enter the x1, x2, y1 and y2 coordinates to calculate their distance\n");
-x1 = getval();
-x2 = getval();
-y1 = getval();
-y2 = getval();
-distance = dist(x1, x2, y1, y2);
-disp(distance);
-return 0;
+    float x1, x2, y1, y2, distance;
+    getval(&x1, &x2, &y1, &y2);
+    distance = dist(x1, x2, y1, y2);
+    disp(distance);
+    return 0;
 }
