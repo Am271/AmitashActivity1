5da119b47e6933204a2e3f8ce37d00e9e892d0d1
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-02-18 23:36:33 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,34 +1,38 @@
 //Write a program to find the sum of n different numbers using 4 functions
-#include<stdio.h>
+#include <stdio.h>
 void getnum(int a[], int n)
 {
-printf("Enter the numbers to be added: ");
-for(int i=0;i<n;i++)
-{
-scanf("%d", &a[i]);
-}
+    printf("Enter the numbers to be added: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
 }
 int add(int a[], int n)
 {
-int sum = 0;
-for(int i=0;i<n;i++)
-{
-sum += a[i];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+    }
+    return sum;
 }
-return sum;
+void getn(int *n)
+{
+    printf("How many numbers do you want to add? ");
+    scanf("%d", n);
 }
 void disp(int a)
 {
-printf("The sum of the entered numbers is %d\n", a);
+    printf("The sum of the entered numbers is %d\n", a);
 }
 int main()
 {
-int n, sum;
-printf("How many numbers do you want to add? ");
-scanf("%d", &n);
-int a[n];
-getnum(a, n);
-sum = add(a, n);
-disp(sum);
-return 0;
+    int n, sum;
+    getn(&n);
+    int a[n];
+    getnum(a, n);
+    sum = add(a, n);
+    disp(sum);
+    return 0;
 }
