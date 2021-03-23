c7cfc638a369206978b241306222ee88b007e307
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-03-23 16:41:52 +0530

    Update rectangle_area.c

--- rectangle_area.c
+++ rectangle_area.c
@@ -1,29 +1,38 @@
 #include<stdio.h>
 #include<math.h>
+
 typedef struct set {
 	float x1, x2, x3, y1, y2, y3, area;
 }coord;
 
-int inp() {
-	int n;
-	scanf("%d", &n);
-	return n;
+void inp(int *n) {
+	scanf("%d", n);
 }
 
 void getcoord(coord *b) {
-	scanf("%d%d%d%d%d%d", &b->x1, &b->y1, &b->x2, &b->y2, &b->x3, &b->y3);
+	scanf("%f%f%f%f%f%f", &b->x1, &b->y1, &b->x2, &b->y2, &b->x3, &b->y3);
+}
+
+float get_dist(float a1, float a2, float b1, float b2) {
+	return sqrt(pow((b2 - b1), 2) + pow((a2 - a1), 2));
 }
 
 void cal_area(coord *c) {
-	float len, hgt;
-	len = sqrt(pow((c->y2 - c->y1), 2) + pow((c->x2 - c->x1), 2));
-	hgt = sqrt(pow((c->y3 - c->y2), 2) + pow((c->x3 - c->x2), 2));
-c->area = len * hgt;
+	float t1, t2, t3;
+	t1 = get_dist(c->y1, c->y2, c->x1, c->x2);
+	t2 = get_dist(c->y1, c->y3, c->x1, c->x3);
+	t3 = get_dist(c->y3, c->y2, c->x3, c->x2);
+	c->area = (t1 > t2 && t1 > t3)? t2 * t3 : ((t2 > t3 && t2 > t1)? t1 * t3 : t2 * t1);
+}
+
+void disp(coord a) {
+	printf("Area of rectangle with vertices (%.1f,%.1f), (%.1f,%.1f), (%.1f,%.1f) is %.1f\n",a.x1, a.y1, a.x2, a.y2, a.x3, a.y3, a.area);
 }
 
 int main() {
-	int n = inp();
+	int n;
 	coord a[75];
+	inp(&n);
 	for(int i=0;i<n;i++) {
 		getcoord(&a[i]);
 	}
@@ -31,4 +40,5 @@ int main() {
 		cal_area(&a[i]);
 		disp(a[i]);
 	}
+	return 0;
 }
