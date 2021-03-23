434bc63ac2c4b2bc7755270d729fe4c051b8af8e
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-03-23 16:44:17 +0530

    Update payroll_processing.c

--- payroll_processing.c
+++ payroll_processing.c
@@ -18,44 +18,62 @@ void inp_emp(Employee *a) {
 	scanf("%f", &a->hourly_wage);
 }
 
+int compare(char a[], char b[]) {
+	int i = 0, c = 0;
+	while(a[i] != '\0' && b[i] != '\0') {
+		if(a[i] == b[i])
+			c++;
+		i++;
+	}
+	if(i == c)
+		return 1;
+	else
+		return 0;
+}
+
 void inp_timecards(Employee a[], int m, int n) {
-	char temp[50]; int mins; int k = 0, j = 0;
+	char temp[50]; int mins; int k = 0, j = 0, flag;
 	for(int i=0;i<m;i++) {
 		scanf("%s", temp);
 		scanf("%d", &mins);
-		if(a[j].name == temp) {
-			a[j].mins_worked[k] = mins;
-			k++;
-		}
-		else {
-			j++; k = 0;
-		}
+		do {
+			if(compare(a[j].name, temp)) {
+				a[j].mins_worked[k] = mins;
+				k++; flag = 0;
+			}
+			else {
+				j++; k = 0; flag = 1;
+			}
+		}while(flag);
 	}
 }
 
 int get_sum(int a[]) {
-	int i = 0, sum = 0;
-	while(a[i] != 0)
+	int i=0, sum = 0;
+	while(a[i] != 0) {
 		sum += a[i];
+		i++;
+	}
 	return sum;
 }
 
 void compute_pay(Employee *a) {
+	float hrs;
 	if(a->mins_worked[0] != 0) {
-		float hrs = get_sum(a->mins_worked) / 60;
+		hrs = (float)get_sum(a->mins_worked) / 60;
 		a->hours_worked = hrs;
 		if(hrs > 40) {
 			hrs = hrs - 40;
 			a->pay = (a->hourly_wage * 40) + (a->hourly_wage * 1.5 * hrs);
 		}
-		else {
+		else
 			a->pay = a->hourly_wage * hrs;
-		}
 	}
 }
 
 void disp(Employee x) {
-	printf("%s: %.2f hours, $%.2f\n", x.name, x.hours_worked, x.pay);
+	if(x.pay != 0)
+		printf("%s: %.2f hours, $%.2f\n", x.name, x.hours_worked, x.pay);
 }
 
 int main() {
@@ -70,3 +88,5 @@ int main() {
 		compute_pay(&emps[j]);
 		disp(emps[j]);
 	}
+	return 0;
+}
